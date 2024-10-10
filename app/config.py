8f1bfd5fc769c9e32208faf26fe49df9bfdab503
d4vidmanu <david.silva.m@utec.edu.pe>
2024-10-09 23:17:22 -0500

    adadad

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "utec")
-DB_HOST = os.getenv("DB_HOST", "34.195.53.21")  # Nueva IP
+DB_HOST = os.getenv("DB_HOST", "18.213.167.234")  # Nueva IP
 DB_NAME = os.getenv("DB_NAME", "gestion_usuarios")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{5432}/{DB_NAME}"
